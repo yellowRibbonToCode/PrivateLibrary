50b44056f4993e167d39f5bc5a410d844a47a27c
hekang <hekang42@student.42seoul.kr>
2021-10-21 17:10:33 +0900

    Add Tab View

--- PrivateLibrary/ContentView.swift
+++ PrivateLibrary/ContentView.swift
@@ -7,13 +7,29 @@
 // YelloRibbontoCode
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().barTintColor = UIColor(Color(red: 136/255, green: 83/255, blue: 24/255)) // custom color.
+        UITabBar.appearance().tintColor = UIColor(Color.red)
+    }
+    
+    @State private var selection = false;
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Text("featured").tabItem {
+                Image(systemName: "book.fill")
+                Text("First")
+            }
+            Text("list").tabItem {
+                Image(systemName: "person.fill")
+                Text("List") }
+        }
+        .accentColor(.white)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
