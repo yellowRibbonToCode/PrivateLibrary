fd25e09192cd1a035b2f4e1c4ccb7bbb9f5ef2b7
hekang <hekang42@student.42seoul.kr>
2021-10-25 19:14:43 +0900

    modify detail view

---
+++ Extension/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  PrivateLibrary
+//
+//  Created by 강희영 on 2021/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let mainBrown = Color("mainBrown")
+}
--- PrivateLibrary/ContentView.swift
+++ PrivateLibrary/ContentView.swift
@@ -14,15 +14,15 @@ struct ContentView: View {
     @State var text: String = "";
     @State var showComposerRuler : Bool = false;
     init() {
-//        UITabBar.appearance().shadowImage = UIImage()
-//        UITabBar.appearance().backgroundColor = UIColor(Color(red: 136/255, green: 83/255, blue: 24/255))
-//            UITabBar.appearance().tintColor = UIColor(Color.red)
-        UITabBar.appearance().barTintColor = UIColor(Color(red: 136/255, green: 83/255, blue: 24/255)) // custom color.
+        //        UITabBar.appearance().shadowImage = UIImage()
+        //        UITabBar.appearance().backgroundColor = UIColor(Color(red: 136/255, green: 83/255, blue: 24/255))
+        //            UITabBar.appearance().tintColor = UIColor(Color.red)
+        UITabBar.appearance().barTintColor = UIColor(Color.mainBrown) // custom color.
     }
     var items = Product.sampleList
     
     var body: some View {
-       
+        
         
         NavigationView {
             TabView(selection: $selection) {
@@ -34,28 +34,30 @@ struct ContentView: View {
                             .clipShape(Circle())
                             .overlay(Circle().stroke(Color.white, lineWidth: 4))
                             .shadow(radius: 7)
-                            .frame(width:30, height:30)
-                        
-                        Text(item.name)
-        //
-                        Text("\(item.price)")
+                            .frame(width:50, height:50)
+                        VStack (alignment: .leading) {
+                            HStack {
+                                Text(item.name)
+                                    .lineLimit(1)
+                                    .truncationMode(.tail)
+                            }
+                            .foregroundColor(.mainBrown)
+                            .multilineTextAlignment(.leading)
+                            .font(.headline)
+                            Text(item.summary)
+                                .font(.subheadline)
+                        }
+                        .padding([.leading])
                         Text(item.category)
                             .padding(5)
-                            .background(Color.gray)
+                            .background(Color.gray.opacity(0.6))
                             .clipShape(Rectangle())
                             .cornerRadius(10)
-//                            .overlay(
-//                                Ellipse()
-//                                .stroke(Color.gray, lineWidth: 4)
-//
-//                            )
                             .allowsTightening(true)
                             .foregroundColor(.white)
-                            
                             .frame(maxWidth: .infinity, alignment: .trailing)
-                            
-                            
                     }
+                    .padding()
                 }
                 .tabItem{
                     Image(systemName: "book.fill")
@@ -67,55 +69,46 @@ struct ContentView: View {
             }
             .accentColor(.white)
             .navigationTitle("주변 도서관")
-            .toolbar(content: {
-                ToolbarItemGroup (placement: .principal) {
-                    ModalButtonMagnifyingGlass(show: $showComposer, text: $text)
-                }
-            })
-            //            .navigationBarItems(leading: ModalButtonMagnifyingGlass(show: $showComposer, text: $text), trailing: ModalButtonRuler(show: $showComposerRuler))
-            //
+//            .toolbar(content: {
+//                ToolbarItemGroup (placement: .principal) {
+//                    ModalButtonMagnifyingGlass(show: $showComposer, text: $text)
+//                }
+//            })
             .navigationBarItems(trailing: ModalButtonRuler(show: $showComposerRuler))
         }
     }
 }
 
-fileprivate struct ModalButtonMagnifyingGlass: View {
-    @Binding var show: Bool
-    @Binding var text : String
-    
-    var body: some View {
-        HStack {
-            TextField("검색어를 넣어주세요" , text : self.$text)
-                .multilineTextAlignment(.center)
-                .padding([.top, .bottom], 7)
-                .frame(minWidth:300)
-                .background(Color(.systemGray6))
-                .cornerRadius(50)
-                .overlay(
-                    HStack{
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(Color(UIColor(Color(red: 136/255, green: 83/255, blue: 24/255))))
-                            .padding()
-                        Spacer()
-                    })
+//fileprivate struct ModalButtonMagnifyingGlass: View {
+//    @Binding var show: Bool
+//    @Binding var text : String
 //
-//                    Button(action: {self.show = true}, label: {
-//                        Image(systemName: "magnifyingglass.circle")
+//    var body: some View {
+//        HStack {
+//            TextField("검색어를 넣어주세요" , text : self.$text)
+//                .multilineTextAlignment(.center)
+//                .padding([.top, .bottom], 7)
+//                .frame(minWidth:300)
+//                .background(Color(.systemGray6))
+//                .cornerRadius(50)
+//                .overlay(
+//                    HStack{
+//                        Image(systemName: "magnifyingglass")
+//                            .foregroundColor(Color.mainBrown)
+//                            .padding()
+//                        Spacer()
 //                    })
-                    
-            //                    .accentColor(Color(UIColor(Color(red: 136/255, green: 83/255, blue: 24/255))))
-            //            })
-        }
-    }
-}
+//        }
+//    }
+//}
 
 fileprivate struct ModalButtonRuler: View {
     @Binding var show: Bool
     
     var body: some View {
         Button(action: {self.show = true}, label: {
-            Image(systemName: "ruler")
-                .foregroundColor(Color(red: 136/255, green: 83/255, blue: 24/255))
+            Image(systemName: "list.bullet")
+                .foregroundColor(.mainBrown)
         })
     }
 }
