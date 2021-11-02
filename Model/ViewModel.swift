39fa574caf56a3ee9f287016c6e2f8c18221f56d
hekang <42266439+hekang42@users.noreply.github.com>
2021-11-02 21:40:28 +0900

    add Viewmodel HOTFIX (#40)
    * fix merge err
    
    * modify add image picker in AddBookInfo View #33
    
    * updated Viewmodel
    
    Co-authored-by: hekang <hekang42@student.42seoul.kr>

--- Model/ViewModel.swift
+++ Model/ViewModel.swift
@@ -10,36 +10,22 @@ import SwiftUI
 import CoreLocation
 
 
-struct ViewModel: Hashable, Codable, Identifiable{
-    var id: Int                     // 고유 코드
-    var bookName: String            // 책 제목
-    var bookWriter: String          // 책 저자
+struct ViewModel: Identifiable{
+    var id: String                     // 고유 코드
     var name: String                // 작성자
     var email: String               // 이메일
-    var abstract: String            // 한줄 요약
-    var content: String             // 게시글 내용
-    var isbn: Int                   // ISBN(Book id)
-
-    var createdDate: Date           // 작성일 Date
-    var editedDate: Date            // 수정일 Date
-    private var imageName: String   // 이미지
-    var transitDate: Date           // 거래 완료일 Date
-    var price: Int                  // 가격
-    var isChangable: Bool           // 교환 가능 여부
-    var isTradable: Bool            // 판매 가능 여부
-    var image: Image {
-        Image(imageName)
-    }
     
-//    private var coordinates: Coordinates
-//    var locationCoordinate: CLLocationCoordinate2D{
-//        CLLocationCoordinate2D(latitude: coordinates.latitude
-//                               , longitude: coordinates.longitude)
-//    }
-//
-//    struct Coordinates: Hashable, Codable {
-//        var latitude: Double
-//        var longitude: Double
-//    }
+    var bookname: String            // 책 제목
+    var author: String          // 책 저자
+    var title: String            // 한줄 요약
+    var content: String             // 게시글 내용
+
+    var created: Date           // 작성일 Date
+    var edited: Date            // 수정일 Date
+
+    var price: Int?                  // 가격
+    var exchange: Bool           // 교환 가능 여부
+    var sell: Bool            // 판매 가능 여부
+    var image: Image?
     
 }
