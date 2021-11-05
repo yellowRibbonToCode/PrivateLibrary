8445f49af0b93e61c994bd96aea66324982d9e8b
hekang <42266439+hekang42@users.noreply.github.com>
2021-11-05 12:44:25 +0900

    Fix LibModel add uid. (#53)
    Co-authored-by: hekang <hekang42@student.42seoul.kr>

--- Views/ImageRow.swift
+++ Views/ImageRow.swift
@@ -78,7 +78,8 @@ struct ImageRow: View {
 
 struct ImageRow_Previews: PreviewProvider {
     static var previews: some View {
-        ImageRow(libModel: ViewModel(id: "book_uid" ,
+        ImageRow(libModel: ViewModel(id:UUID(),
+                                     useruid: "book_uid" ,
                                      name: "hekang",
                                      email: "hekang@hekang.com",
                                      bookname: "전문가를 위한 파이썬",
