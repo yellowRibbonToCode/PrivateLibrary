db700faec019da9a51bda9c6380a7ab84e08d37f
hekang <42266439+hekang42@users.noreply.github.com>
2021-11-08 17:19:06 +0900

    Add : PullDownToRefresh #49 (#70)
    * PullDownToRefresh
    
    * Commit Test
    
    Co-authored-by: hekang42 <hekang42@student.42seoul.kr>

---
+++ Extension/PulltoRefresh.swift
@@ -0,0 +1,42 @@
+//
+//  PulltoRefresh.swift
+//  PrivateLibrary
+//
+//  Created by 강희영 on 2021/11/08.
+//
+
+import SwiftUI
+
+struct PullToRefresh: View {
+    
+    var coordinateSpaceName: String
+    var onRefresh: ()->Void
+    
+    @State var needRefresh: Bool = false
+    
+    var body: some View {
+        GeometryReader { geo in
+            if (geo.frame(in: .named(coordinateSpaceName)).midY > 50) {
+                Spacer()
+                    .onAppear {
+                        needRefresh = true
+                    }
+            } else if (geo.frame(in: .named(coordinateSpaceName)).maxY < 10) {
+                Spacer()
+                    .onAppear {
+                        if needRefresh {
+                            needRefresh = false
+                            onRefresh()
+                        }
+                    }
+            }
+            HStack {
+                Spacer()
+                if needRefresh {
+                    ProgressView()
+                }
+                Spacer()
+            }
+        }.padding(.top, -50)
+    }
+}
--- Views/ImageGridView.swift
+++ Views/ImageGridView.swift
@@ -15,7 +15,6 @@ class SearchViewModel: ObservableObject {
     @Published var bookModels = [ViewModel]()
     @Published var bookImage = Image(systemName: "book")
     
-    
     func makeList() {
         db.collection("libData").getDocuments() {
             (books, err) in
@@ -87,6 +86,11 @@ struct ImageGridView: View {
     var body: some View {
         NavigationView {
             ScrollView(.vertical) {
+                PullToRefresh(coordinateSpaceName: "pullToRefresh"){
+                    searchViewModel.makeList()
+                }
+                .padding(.top, -50)
+                
                 LazyVGrid(columns: columns) {
                     ForEach ( searchViewModel.bookModels.sorted { $0.created!.compare($1.created!) == .orderedDescending} ) {
                         Model in
@@ -98,12 +102,14 @@ struct ImageGridView: View {
                         .foregroundColor(.black)
                     }
                 }
+                
+                .padding([.leading, .trailing], 10)
                 .onAppear(perform: {
                     db = Firestore.firestore()
                     searchViewModel.makeList()
                 })
-                .padding([.leading, .trailing], 10)
             }
+            .coordinateSpace(name: "pullToRefresh")
             .navigationBarTitle(Text("Books"))
         }
 //        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
