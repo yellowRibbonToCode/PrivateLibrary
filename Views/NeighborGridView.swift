faa80edd06cd8c3fbb1e7c08d6da7b6b405c6c53
hekang42 <hekang@student.42seoul.kr>
2021-11-13 11:26:52 +0900

    get NeighborList

--- Views/NeighborGridView.swift
+++ Views/NeighborGridView.swift
@@ -13,58 +13,117 @@ import FirebaseStorage
 
 class NeighborViewModel: ObservableObject {
     @Published var bookModels = [ViewModel]()
-    @Published var bookImage = Image(systemName: "book")
+    //    @Published var bookImage = Image(systemName: "book")
+    private let userid = Auth.auth().currentUser!.uid
+    private let db = Firestore.firestore()
+    private var userLatitude = ""
+    private var userLongitude = ""
+    @Published var neighborList = [String]()
+    private var range = 100.0 // 100km
     
-    func makeList() {
-        db.collection("libData").getDocuments() {
-            (books, err) in
-            if let err = err{
-                print("Error getting documents: \(err)")
-            }
-            else {
-                guard let books = books?.documents else { return }
-                self.bookModels = [ViewModel]()
-                for book in books {
-                    func getImage(bookuid : String) {
-                        Storage.storage().reference().child("images/books/\(bookuid)").getData(maxSize: 100 * 200 * 200) {
-                            (imageData, err) in
-                            if let err = err as NSError? {
-                                let randInt = Int.random(in: 0...10)
-                                print(err)
-                                self.bookModels.append(ViewModel(id: bookuid, useruid: book.get("userid") as! String ,
-                                                                 name: book.get("username") as! String,
-                                                                 email: book.get("useremail") as! String,
-                                                                 bookname: book.get("bookname") as! String,
-                                                                 author: book.get("author") as! String,
-                                                                 title: book.get("title") as! String,
-                                                                 content: book.get("content") as! String,
-                                                                 created: (book.get("created") as! Timestamp).dateValue(),
-                                                                 edited: (book.get("edited") as! Timestamp).dateValue(),
-                                                                 price: book.get("price") as? Int,
-                                                                 exchange: book.get("exchange") as! Bool,
-                                                                 sell: book.get("sell") as! Bool,
-                                                                 image: Image(RandBookImage(rawValue: randInt)!.toString())))
-                                }
-                        }
-                    }
-                    getImage(bookuid: book.documentID)
+    
+    
+    func getUserLocation() {
+        db.collection("users").document(userid).getDocument { [self] user, err in
+            if let user = user {
+                self.userLatitude = user.get("latitude") as! String
+                self.userLongitude = user.get("longitude") as! String
+            } else {
+                if let err = err {
+                    print("Error getting documents: \(err)")
                 }
             }
-            print(self.bookModels)
         }
-        print(bookModels)
+    }
+    
+    
+    func makeNeighborList() async throws{
+        getUserLocation()
+        self.neighborList = []
+        let _ = db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    self.db.collection("users").document(document.documentID).getDocument { userData, err in
+                        if let err = err {
+                            print("Error getting document: \(err)")
+                        }
+                        else if let userData = userData {
+                            let tempLatitude = userData.get("latitude") as? String ?? ""
+                            let tempLongitude = userData.get("longitude") as? String ?? ""
+                            let distance = getDistance(latitude1: self.userLatitude, longitude1: self.userLongitude, latitude2: tempLatitude, longitude2: tempLongitude)
+                            if (distance < self.range && distance > 0)
+                            {
+                                print("append")
+                                self.neighborList.append(document.documentID)
+                                //                                print("documentID : \(document.documentID)")
+                                print(self.neighborList)
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+    }
+    
+    func makeNeighborBookList() {
+//        let _ = try await makeNeighborList()
+        self.bookModels = [ViewModel]()
+        for uid in self.neighborList {
+            print(uid)
+            db.collection("libData").whereField("userid", isEqualTo: uid).getDocuments() {
+                (books, err) in
+                if let err = err{
+                    print("Error getting documents: \(err)")
+                }
+                else {
+                    guard let books = books?.documents else { return }
+                    
+                    for book in books {
+                        func getImage(bookuid : String) {
+                            Storage.storage().reference().child("images/books/\(bookuid)").getData(maxSize: 100 * 200 * 200) {
+                                (imageData, err) in
+                                if let err = err as NSError? {
+                                    let randInt = Int.random(in: 0...10)
+                                    print(err)
+                                    self.bookModels.append(ViewModel(id: bookuid, useruid: book.get("userid") as! String ,
+                                                                     name: book.get("username") as! String,
+                                                                     email: book.get("useremail") as! String,
+                                                                     bookname: book.get("bookname") as! String,
+                                                                     author: book.get("author") as! String,
+                                                                     title: book.get("title") as! String,
+                                                                     content: book.get("content") as! String,
+                                                                     created: (book.get("created") as! Timestamp).dateValue(),
+                                                                     edited: (book.get("edited") as! Timestamp).dateValue(),
+                                                                     price: book.get("price") as? Int,
+                                                                     exchange: book.get("exchange") as! Bool,
+                                                                     sell: book.get("sell") as! Bool,
+                                                                     image: Image(RandBookImage(rawValue: randInt)!.toString())))
+                                }
+                            }
+                        }
+                        getImage(bookuid: book.documentID)
+                    }
+                }
+                print(self.bookModels)
+            }
+        }
     }
 }
 
 
 struct NeighborGridView: View {
     let columns: [GridItem] = Array(repeating: GridItem(), count: 2)
-    @ObservedObject var searchViewModel = SearchViewModel()
+    @ObservedObject var searchNeighborViewModel = NeighborViewModel()
     var body: some View {
         NavigationView {
+            Text("hello")
             ScrollView(.vertical) {
                 LazyVGrid(columns: columns) {
-                    ForEach ( searchViewModel.bookModels.sorted { $0.created!.compare($1.created!) == .orderedDescending} ) {
+                    ForEach ( searchNeighborViewModel.bookModels.sorted { $0.created!.compare($1.created!) == .orderedDescending} ) {
                         Model in
                         NavigationLink(destination: DetailView(libModel: Model)) {
                             ImageRow(libModel: Model)
@@ -78,8 +137,20 @@ struct NeighborGridView: View {
                 .padding([.leading, .trailing], 10)
                 .onAppear(perform: {
                     db = Firestore.firestore()
-                    searchViewModel.makeList()
+//                    do {
+                    Task.init {
+                        let _ = try await searchNeighborViewModel.makeNeighborList()
+                        
+                    }
+                    searchNeighborViewModel.makeNeighborBookList()
+//                    }
+//                    catch {
+//                        print("error")
+//                    }
+                    //                    searchNeighborViewModel.getUserLocation()
+                    //                    searchNeighborViewModel.makeNeighborList()
                 })
+                
             }
             .navigationBarTitle(Text("Books"))
         }
