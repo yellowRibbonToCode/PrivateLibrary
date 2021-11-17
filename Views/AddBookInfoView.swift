f1f721c07985b0f3691239f1e85b297dd84b3cf5
sushin-42 <sushin@student.42seoul.kr>
2021-11-18 04:00:04 +0900

    add book page working (temp)

--- Views/AddBookInfoView.swift
+++ Views/AddBookInfoView.swift
@@ -31,12 +31,12 @@ struct AddBookInfoView: View {
     
     let userId = Auth.auth().currentUser?.uid ?? "userId"
     
-    @State var username : String = " "
-    @State var email : String = " "
-    @State var author : String = " "
-    @State var bookname : String = " "
-    @State var title : String = " "
-    @State var content : String = " "
+    @State var username : String = ""
+    @State var email : String = ""
+    @State var author : String = ""
+    @State var bookname : String = ""
+    @State var title : String = ""
+    @State var content : String = ""
     @State var created : Date = Date()
     @State var edited : Date = Date()
     @State var price : Int = 0
@@ -126,118 +126,192 @@ struct AddBookInfoView: View {
     }
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    
+    @State var tapped: Bool = false
+
     
     var body: some View {
         
-        VStack(alignment: .center, spacing: 10) {
-            HStack {
-                Button(action:
-                {
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    
-                    Image(systemName: "arrow.left")
-                }
-                    
-            }
+        VStack {
+
             Button(action: {
                 self.isShowPhotoLibrary = true
             }) {
                 if image != nil {
                     Image(uiImage: image!)
                         .resizable()
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 200)
-                        .shadow(radius: 10)
+                        .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height * 0.48)
                 }
                 else {
-                    VStack {
+
                         Image(systemName: "plus.app")
                             .font(.system(size: 20))
-                        Text("Add Image")
-                    }
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 200)
-                    .background(Color.blue.opacity(0.1))
-                    .foregroundColor(.gray)
-                    .cornerRadius(20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 1))
+                            .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height * 0.48)
+                            .background(Color.blue.opacity(0.1))
+                            .foregroundColor(.gray)
+
                 }
             }
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Bookname")
-                    .foregroundColor(.gray)
-                TextField("Enter the bookname", text: $bookname)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Author")
-                    .foregroundColor(.gray)
-                TextField("Enter the author", text: $author)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Title")
-                    .foregroundColor(.gray)
-                TextField("Enter the title", text: $title)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            HStack{
-                Button(action: {
-                    exchange = exchange ? false : true
-                    
-                }) {
-                    HStack(alignment: .top, spacing: 10) {
-                        
-                        Image(systemName: self.exchange ? "checkmark.square" : "square")
-                        Text("Exchange")
-                    }
-                }
-                Button(action: {
-                    sell = sell ? false : true
-                    
-                }) {
-                    HStack(alignment: .top, spacing: 10) {
-                        
-                        Image(systemName: self.sell ? "checkmark.square" : "square")
-                        Text("Sell")
-                    }
-                }
-            }
-            .foregroundColor(.gray)
+
             
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Price")
-                    .foregroundColor(.gray)
-                TextField("Enter the price", value: $price, formatter: NumberFormatter()) //  .string(from: NSNumber(value:price))!
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Content")
-                    .foregroundColor(.gray)
-                TextField("Enter the content", text: $content)
-                    .fixedSize(horizontal: false, vertical: false)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-//                    .frame(height: 150)
-                    
-////                    .padding([.horizontal], 4)
-//                    .cornerRadius(10)
-//                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray).opacity(0.3))
-////                    .padding([.horizontal], 24)
-            }
-            
-            Button(action: {
-                setdb()
-                self.presentationMode.wrappedValue.dismiss()
+            VStack(alignment: .leading, spacing: 0){
+                // Top
                 
-            }) {
-                Text("Add New Book")
-                    .foregroundColor(.blue)
+                TextField("도서명", text: $bookname)
+                .padding(EdgeInsets(top: 36, leading: 16, bottom: 10, trailing: 32))
+                .font(Font.custom("S-CoreDream-6Bold", size: 33))
+                
+                
+               // Middle
+                
+                VStack (spacing: 0) {
+                    
+                    HStack {
+                        TextField("작가", text: $author)
+                            .font(Font.custom("S-CoreDream-6Bold", size: 20))
+                        Spacer()
+                    }
+                    HStack {
+                        Image(systemName: "quote.opening")
+                            .resizable()
+                            .frame(width: 20, height: 15)
+                            .padding(.bottom, UIScreen.main.bounds.height / 18)
+                        TextField("제목을 입력해 주세요", text: $title)
+                            .font(Font.custom("S-CoreDream-6Bold", size: 18))
+                            .padding(.horizontal, 34).multilineTextAlignment(.center)
+                        Image(systemName: "quote.closing")
+                            .resizable()
+                            .frame(width: 20, height: 15)
+                            .padding(.top, UIScreen.main.bounds.height / 18)
+                    }                .padding(.horizontal, 10)
+
+                    .frame(height: UIScreen.main.bounds.height / 9)
+                    .foregroundColor(.mainBlue)
+                }
+                .padding(.horizontal)
+                
+               // Bottom
+                
+                HStack {
+                    Text("username의 시선")
+                        .font(Font.custom("S-CoreDream-6Bold", size: 16))
+                        .foregroundColor(.mainBlue)
+                    Spacer()
+                }.padding(EdgeInsets(top: 0, leading: 16, bottom: 3, trailing: 16))
+//                ScrollView{
+//                    ZStack (alignment: .leading){
+                        TextEditor(text: $content)
+                            .font(Font.custom("S-CoreDream-3Light", size: 15))
+                            .padding()
+                            .lineSpacing(3).background(Color.gray)
+//                            .overlay(Rectangle()
+//                                                        .fill(Color(red: 0.745, green: 0.745, blue: 0.745, opacity: 0.15))
+//                                                        .clipShape(contentRounded()))
+//                        Rectangle()
+//                            .fill(Color(red: 0.745, green: 0.745, blue: 0.745, opacity: 0.15))
+//                            .clipShape(contentRounded())
+//                    }
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 16, trailing: 16))
+//                }
             }
-            .padding()
-            Spacer()
+            .background(Color.white)
+            .clipShape(Rounded())
+            .padding(.top, self.tapped ? -UIScreen.main.bounds.height / 2.5 : -UIScreen.main.bounds.height / 12)
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    self.tapped.toggle()
+                }
+            }
+            VStack{
+            // Addbook button
+                Button(action: {
+                    setdb()
+                    self.presentationMode.wrappedValue.dismiss()
+                    
+                }) {
+                    Text("작성 완료")
+                        .frame(width: UIScreen.main.bounds.width / 1.7, height: 35)
+                           .background(Color.white)
+                           .cornerRadius(20)
+                           .foregroundColor(.mainBlue)
+                           .clipShape(RoundedRectangle(cornerRadius: 10))
+                           .overlay(
+                               RoundedRectangle(cornerRadius: 30)
+                                .stroke(Color.mainBlue, lineWidth: 2.5)
+                           )
+                }
+                .padding()
+            }
+      
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("Bookname")
+//                    .foregroundColor(.gray)
+//                TextField("Enter the bookname", text: $bookname)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//            }
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("Author")
+//                    .foregroundColor(.gray)
+//                TextField("Enter the author", text: $author)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//            }
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("Title")
+//                    .foregroundColor(.gray)
+//                TextField("Enter the title", text: $title)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//            }
+//            HStack{
+//                Button(action: {
+//                    exchange = exchange ? false : true
+//
+//                }) {
+//                    HStack(alignment: .top, spacing: 10) {
+//
+//                        Image(systemName: self.exchange ? "checkmark.square" : "square")
+//                        Text("Exchange")
+//                    }
+//                }
+//                Button(action: {
+//                    sell = sell ? false : true
+//
+//                }) {
+//                    HStack(alignment: .top, spacing: 10) {
+//
+//                        Image(systemName: self.sell ? "checkmark.square" : "square")
+//                        Text("Sell")
+//                    }
+//                }
+//            }
+//            .foregroundColor(.gray)
+//
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("Price")
+//                    .foregroundColor(.gray)
+//                TextField("Enter the price", value: $price, formatter: NumberFormatter()) //  .string(from: NSNumber(value:price))!
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//            }
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("Content")
+//                    .foregroundColor(.gray)
+//                TextField("Enter the content", text: $content)
+//                    .fixedSize(horizontal: false, vertical: false)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//
+//            }
+
         }
+        
         .sheet(isPresented: $isShowPhotoLibrary) {
             ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
                 .padding()
@@ -245,12 +319,33 @@ struct AddBookInfoView: View {
                     db = Firestore.firestore()
                 })
         }
-        .padding(EdgeInsets(top: 20, leading: 40, bottom: 0, trailing: 40))
-      
+//        .navigationBarBackButtonHidden(true)
+//            .navigationBarItems(leading: btnBack)
+
+            .edgesIgnoringSafeArea(.all)
+
+
     }
+    
+//    var btnBack : some View
+//    {
+//        Button(action: {
+//            self.presentationMode.wrappedValue.dismiss()
+//        }) {
+//            Image(systemName: "arrow.left")
+//                .aspectRatio(contentMode: .fit)
+//                .foregroundColor(.white)
+//        }
+//    }
 }
+
 struct AddBookInfoView_Previews: PreviewProvider {
     static var previews: some View {
         AddBookInfoView()
     }
 }
+
+//    .navigationBarBackButtonHidden(true)
+//        .navigationBarItems(leading: btnBack)
+//
+//        .edgesIgnoringSafeArea(.all)
