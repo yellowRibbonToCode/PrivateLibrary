5cb253929996ee754c7ceb00a1bc7de48c658bc8
sushin-42 <72022736+sushin-42@users.noreply.github.com>
2021-11-17 23:06:31 +0900

    detailview edit (#112)
    * tapView Design Edit
    
    * detailview edit

--- PrivateLibrary/AppDelegate.swift
+++ PrivateLibrary/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        sleep(2)
         return true
     }
     
