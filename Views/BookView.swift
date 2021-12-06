14dd9b4af183b7f02d57c2e7ee7a89ae8816b2fa
hekang42 <hekang@student.42seoul.kr>
2021-12-06 16:22:07 +0900

    Fix Book View to getImage

--- Views/BookView.swift
+++ Views/BookView.swift
@@ -27,7 +27,7 @@ struct BookView: View {
                 ScrollView(.vertical) {
                     LazyVGrid(columns: columns) {
                         ForEach (books.bookList.sorted { $0.created!.compare($1.created!) == .orderedDescending }) { book in
-                            if (!book.reports!.contains(useruid) && !book.blocks!.contains(useruid)) {
+                            if (book.reports!.count < 2 && !book.blocks!.contains(useruid)) {
                                 NavigationLink(destination: DetailView(libModel: book)) {
                                     ImageRow(libModel: book)
                                 }
@@ -53,23 +53,23 @@ struct BookView_Previews: PreviewProvider {
 
 class GetBookImage: ObservableObject {
     @Published var bookImage = Image(systemName: "book")
-    func getImage(bookuid : String){
+    func getImage(bookuid : String, completion: @escaping (Image) -> Void){
         if let imageData = UserDefaults.standard.data(forKey: bookuid) {
-            self.bookImage = Image(uiImage: UIImage(data: imageData)!)
+            completion(Image(uiImage: UIImage(data: imageData)!))
         } else {
             Storage.storage().reference().child("images/books/\(bookuid)").getData(maxSize: 100 * 200 * 200) {
                 (imageData, err) in
                 if let _ = err as NSError? {
                     let randInt = Int.random(in: 0...13)
-                    self.bookImage = Image(RandBookImage(rawValue: randInt)!.toString())
+                    completion(Image(RandBookImage(rawValue: randInt)!.toString()))
                 }
                 else {
                     let randInt = Int.random(in: 0...13)
                     self.bookImage = Image(RandBookImage(rawValue: randInt)!.toString())
                     if let imageData = imageData {
-                        self.bookImage = Image(uiImage: UIImage(data: imageData)!)
+                        UserDefaults.standard.set(imageData, forKey: bookuid)
+                        completion(Image(uiImage: UIImage(data: imageData)!))
                     }
-                    UserDefaults.standard.set(imageData, forKey: bookuid)
                 }
             }
         }
@@ -87,31 +87,38 @@ class getBookList: ObservableObject {
     func loadBookList() {
         db.collection("libData").addSnapshotListener {
             (snapshot, err) in
-            guard let documents = snapshot?.documents else {
+            guard let documents = snapshot?.documentChanges else {
                 print("Error fetching snapshots: \(err!)")
                 return
             }
-            self.bookList = documents.map{ QueryDocumentSnapshot in
-                let data = QueryDocumentSnapshot.data()
-                let id = QueryDocumentSnapshot.documentID
-                let useruid = data["userid"] as? String ?? ""
-                let name = data["username"] as? String ?? ""
-                let email = data["useremail"] as? String ?? ""
-                let bookname = data["bookname"] as? String ?? ""
-                let author = data["author"] as? String ?? ""
-                let title = data["title"] as? String ?? ""
-                let content = data["content"] as? String ?? ""
-                let created = (data["created"] as? Timestamp)?.dateValue()
-                let edited = (data["edited"] as? Timestamp)?.dateValue()
-                let price = data["price"] as? Int
-                let exchange = data["exchange"] as? Bool ?? false
-                let sell = data["sell"] as? Bool ?? false
-                let blocks = data["blocks"] as? [String] ?? []
-                let reports = data["report"] as? [String] ?? []
-                self.getBookImage.getImage(bookuid: id)
-                let bookimage = self.getBookImage.bookImage
-                return ViewModel(id: id, useruid: useruid, name: name, email: email, bookname: bookname, author: author, title: title, content: content, created: created, edited: edited, price: price, exchange: exchange, sell: sell, blocks:blocks, reports: reports, image: bookimage)
-            }
+            documents.forEach({ QueryDocumentSnapshot in
+                if (QueryDocumentSnapshot.type == .added){
+                    let data = QueryDocumentSnapshot.document.data()
+                    let id = QueryDocumentSnapshot.document.documentID
+                    let useruid = data["userid"] as? String ?? ""
+                    let name = data["username"] as? String ?? ""
+                    let email = data["useremail"] as? String ?? ""
+                    let bookname = data["bookname"] as? String ?? ""
+                    let author = data["author"] as? String ?? ""
+                    let title = data["title"] as? String ?? ""
+                    let content = data["content"] as? String ?? ""
+                    let created = (data["created"] as? Timestamp)?.dateValue()
+                    let edited = (data["edited"] as? Timestamp)?.dateValue()
+                    let price = data["price"] as? Int
+                    let exchange = data["exchange"] as? Bool ?? false
+                    let sell = data["sell"] as? Bool ?? false
+                    let blocks = data["blocks"] as? [String] ?? []
+                    let reports = data["report"] as? [String] ?? []
+                    self.getBookImage.getImage(bookuid: id) {
+                        image in
+                        self.bookList.append(ViewModel(id: id, useruid: useruid, name: name, email: email, bookname: bookname, author: author, title: title, content: content, created: created, edited: edited, price: price, exchange: exchange, sell: sell, blocks:blocks, reports: reports, image: image))
+                    }
+                }
+                else if (QueryDocumentSnapshot.type == .removed) {
+                    let removeIndex = self.bookList.firstIndex(where: {$0.id == QueryDocumentSnapshot.document.documentID})
+                    self.bookList.remove(at: removeIndex!)
+                }
+            })
         }
     }
 }
